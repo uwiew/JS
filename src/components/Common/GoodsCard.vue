4aee0ef3d9fa68532cafa1892b73bbac6c93126e
weiwu <645046307@qq.com>
2017-07-02 15:42:41 +0800

    添加商品样式

--- src/components/Common/GoodsCard.vue
+++ src/components/Common/GoodsCard.vue
@@ -19,9 +19,21 @@ export default {
 </script>
 
 <style lang="stylus">
+$mobile-width = 767px
+
 .goods-card
-  margin: 0
-  width: 100px
+  width 22%
+  margin 0 0 20px
+  transition all .2s linear
+  background #fff
+  padding 30px 0
+  text-align center
+  border 1px solid #eaeefb
+  border-radius 4px
+  cursor pointer
+  &:hover
+    box-shadow 0 15px 30px rgba(0, 0, 0, 0.1)
+    transform translate3d(0, -2px, 0)
   img
     width: 100%
 </style>
