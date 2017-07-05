62b4c1a898eb251710a5f7cfba442ed4859f2e63
weiwu <645046307@qq.com>
2017-07-05 12:00:23 +0800

    完成订单状态样式

--- src/views/OrderStatus.vue
+++ src/views/OrderStatus.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="order-sratus">
-    <el-steps :space="200" :active="2">
-      <el-step id="step1" title="步骤 1" description="您的订单已提交"></el-step>
-      <el-step id="step2" title="步骤 2" description="卖家已发货"></el-step>
-      <el-step id="step3" title="步骤 3" description="确认收货"></el-step>
+    <el-steps :space="100" :active="2" :center="true">
+      <el-step id="step1" title="待发货"></el-step>
+      <el-step id="step2" title="已发货"></el-step>
+      <el-step id="step3" title="已完成"></el-step>
     </el-steps>
   </div>
 </template>
@@ -14,9 +14,9 @@ export default {
 </script>
 <style lang="stylus">
 #step1
-  width 30vw!important
+  width 33vw!important
 #step2
-  width 30vw!important
+  width 33vw!important
 #step3
-  width 50px!important  
+  width 50px!important
 </style>
